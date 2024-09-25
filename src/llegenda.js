fb36b078a3a6723a1a65bc1198e79120f66c0e77
NuriaJulia <n.julia@creaf.uab.cat>
2024-09-25 10:22:25 +0200

    minor encoding correction

--- src/llegenda.js
+++ src/llegenda.js
@@ -1,4 +1,4 @@
-/* 
+﻿/* 
     This file is part of MiraMon Map Browser.
     MiraMon Map Browser is free software: you can redistribute it and/or modify
     it under the terms of the GNU Affero General Public License as published by
